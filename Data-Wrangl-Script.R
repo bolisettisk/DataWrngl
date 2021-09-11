fcdb2bfa62b6ed9e3aa8433345593948d78191d5
bolisettisk <siva.karteek@gmail.com>
2021-09-11 16:37:27 +0100

    .

--- Data-Wrangl-Script.R
+++ Data-Wrangl-Script.R
@@ -11,3 +11,12 @@ library(readxl) #Included in tidyverse package. Don't need to be installed
 # For data stored in Microsoft Excel formats
 
 names(dat)
+
+cat("\014") # clears R console
+
+# Scan function example
+path <-  system.file("extdata", package = "dslabs")
+
+x <- scan(file.path(path, filename), sep = ",", what = "c")
+x[1:10]
+
