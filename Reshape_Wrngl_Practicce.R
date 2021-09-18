24c35feba53d37c995cdd31abefadf38126b4ef3
bolisettisk <siva.karteek@gmail.com>
2021-09-18 23:25:00 +0100

    .

---
+++ Reshape_Wrngl_Practicce.R
@@ -0,0 +1,20 @@
+library(tidyverse, dslabs)
+cat("\014")
+path <- system.file("extdata", package = "dslabs")
+filename <- file.path(path, "fertility-two-countries-example.csv")
+wide_data <- read_csv(filename)
+
+new_tidy_data <- wide_data %>% pivot_longer(-country, names_to = "year", values_to = "fertility") %>% mutate(year = as.integer(year))
+new_tidy_data
+
+new_wide_data <- new_tidy_data %>% pivot_wider(names_from = year, values_from = fertility)
+new_wide_data
+
+cat("\014")
+filename1 <- file.path(path, "life-expectancy-and-fertility-two-countries-example.csv")
+raw_dat <- read_csv(filename1)
+dat <- raw_dat %>% pivot_longer(-country)
+
+dat %>% separate(name, c("year", "name"))
+
+
---
+++ joining-tables.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(dslabs)
+library(ggrepel)
+library(dplyr)
+data("murders")
+head(murders)
+cat("\014")
+
+data("polls_us_election_2016")
+head(results_us_election_2016)
+
+identical(results_us_election_2016$state, murders$state)
+
+tab <- left_join(murders, results_us_election_2016, by = "state") %>% select(-others) %>% rename(ev = electoral_votes)
+cat("\014")
+head(tab)
+cat("\014")
+
+tab %>% ggplot(aes(population/10^6, ev, label = abb)) + geom_point() + geom_text_repel() + scale_x_continuous(trans = "log2") + scale_y_continuous(trans = "log2") + geom_smooth(method = "lm", se = FALSE)
