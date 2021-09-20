ebeb22e50a24d70edf19e7c8d2f45531e1ea5725
bolisettisk <siva.karteek@gmail.com>
2021-09-20 21:49:10 +0100

    .

--- Reshape_Wrngl_Practicce.R
+++ Reshape_Wrngl_Practicce.R
@@ -1,10 +1,14 @@
-library(tidyverse, dslabs)
+library(tidyverse)
+library(dslabs)
 cat("\014")
 path <- system.file("extdata", package = "dslabs")
 filename <- file.path(path, "fertility-two-countries-example.csv")
 wide_data <- read_csv(filename)
 
 new_tidy_data <- wide_data %>% pivot_longer(-country, names_to = "year", values_to = "fertility") %>% mutate(year = as.integer(year))
+#new_tidy_data <- wide_data %>% gather(year, fertility, `1960`:`2015`)
+new_tidy_data <- wide_data %>% gather(year, fertility, -country)
+head(new_tidy_data)
 new_tidy_data
 
 new_wide_data <- new_tidy_data %>% pivot_wider(names_from = year, values_from = fertility)
