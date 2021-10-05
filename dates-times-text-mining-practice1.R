b0278c58eaba07d4a4d0479c47c49b18ef9a3f45
bolisettisk <siva.karteek@gmail.com>
2021-10-05 01:13:30 +0100

    .

---
+++ dates-times-text-mining-practice1.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(dslabs)
+library(lubridate)
+
+data("polls_us_election_2016")
+
+
+polls_us_election_2016 %>% head()
+cat("\014")
+class(polls_us_election_2016$startdate)
+as.numeric(polls_us_election_2016$startdate) %>% head()
+polls_us_election_2016$startdate %>% head()
+
+
+as.Date("1970-01-31") %>% as.numeric()
+
+cat("\014")
+
+polls_us_election_2016 %>% filter(pollster == "Ipsos" &  state == "U.S.") %>% ggplot(aes(startdate, rawpoll_trump)) + geom_line()
+
+
+# Lubridate Package
+
+cat("\014")
+set.seed(2002)
+dates <- sample(polls_us_election_2016$startdate, 10) %>% sort()
+dates
+cat("\014")
+tibble(date = dates, month = month(dates), day = day(dates), year = year(dates))
+
+month(dates, label = TRUE)
+
+
