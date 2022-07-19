79669a6971977c904cd03e510f7109baaaedb2fb
bolisettisk <siva.karteek@gmail.com>
2022-07-19 22:51:41 +0100

    .

--- practice.R
+++ practice.R
@@ -1,6 +1,15 @@
 library(dslabs)
 library(rvest)
 library(dplyr)
+library(stringr)
+
+url <- paste0("https://en.wikipedia.org/w/index.php?title=",
+              "Gun_violence_in_the_United_States_by_state",
+              "&direction=prev&oldid=810166167")
+murders_raw <- read_html(url) %>%
+  html_node("table") %>%
+  html_table() %>%
+  setNames(c("state", "population", "total", "murder_rate"))
 
 data(reported_heights)
 cat("\014")
@@ -19,6 +28,10 @@ not_inches <- function(x, smallest=50, tallest=84){
   ind
 }
 
-cat("\014")
 problems <- reported_heights %>% filter(not_inches(height)) %>% pull(height)
 length(problems)
+
+cat("\014")
+
+pattern <- ","
+str_detect(murders_raw$total, pattern)
